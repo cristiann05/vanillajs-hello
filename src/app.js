0991d1c0227a4b1fff842da7a4cb85d74e45ae0a
Cristian Ayala Sánchez <ayalasanchezcristian2000@gmail.com>
2024-06-14 10:35:56 +0000

    ejercicio completado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let quien = ["El perro", "Mi abuela", "El cartero", "Mi pájaro"];
+  let accion = ["se comió", "orinó", "aplastó", "rompió"];
+  let que = ["mi tarea", "mi teléfono", "el coche"];
+  let cuando = [
+    "antes de la clase",
+    "cuando estaba durmiendo",
+    "mientras hacía ejercicio",
+    "durante mi almuerzo",
+    "mientras estaba rezando"
+  ];
+
+  function excusarandom() {
+    let quienlohizo = Math.floor(Math.random() * quien.length);
+    let accionhecha = Math.floor(Math.random() * accion.length);
+    let quehizo = Math.floor(Math.random() * que.length);
+    let cuandolohizo = Math.floor(Math.random() * cuando.length);
+
+    let excusa =
+      quien[quienlohizo] +
+      " " +
+      accion[accionhecha] +
+      " " +
+      que[quehizo] +
+      " " +
+      cuando[cuandolohizo] +
+      ".";
+
+    document.getElementById("excusa").innerHTML = excusa;
+  }
+
+  excusarandom();
 };
